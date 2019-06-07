6eda0d885705bac0932c2d9d83bce6a9b1a3f5da
seiermann <github@ultraapp.de>
2019-06-07 22:38:02 +0200

    Add ignorefiles; Add Docker build things; app.py delivers full webinterface

---
+++ Dockerfile
@@ -0,0 +1,28 @@
+FROM node:8.16.0-jessie
+
+
+COPY . /app/
+
+RUN cd /app ; \
+    npm install; \
+    npm run build
+
+
+FROM python:3.6.8
+
+COPY --from=0 /app/build/ /usr/src/app/
+
+COPY ./app.py /usr/src/app/
+
+RUN pip3 install Flask==1.0.3 Flask-Caching==1.7.2 Flask-Cors==3.0.7
+
+WORKDIR /usr/src/app/
+
+
+# ENV FLASK_APP=
+ENTRYPOINT ["flask"]
+
+
+CMD ["run", "-h", "0.0.0.0", "-p", "80" ]
+
+
--- app.py
+++ app.py
@@ -1,25 +1,28 @@
-from flask import Flask, make_response, send_file
+from flask import Flask, make_response, send_file, render_template, send_from_directory
 from flask_cors import CORS
 import io
 from flask_caching import Cache
+from werkzeug.routing import BaseConverter
+import os
 
 
-app = Flask(__name__)
+class RegexConverter(BaseConverter):
+    def __init__(self, url_map, *items):
+        super(RegexConverter, self).__init__(url_map)
+        self.regex = items[0]
+
+
+app = Flask(__name__, static_url_path='')
 cache = Cache(app,config={'CACHE_TYPE': 'simple'})
 cache.init_app(app)
 
 CORS(app)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
-
-
 @cache.cached(timeout=50)
 @app.route("/api/blob/get")
 def api_blob_get():
 
-    with open("./data.mrmp", "rb") as fobj:
+    with open(os.environ.get("MRMP_FILE", "./data.mrmp", "rb") as fobj:
         bin_data = io.BytesIO(fobj.read())
 
     return send_file(
@@ -28,5 +31,17 @@ def api_blob_get():
         mimetype='application/x-msgpack'
     )
 
+app.url_map.converters['regex'] = RegexConverter
 
+@app.route(r'/<regex(".*"):path>')
+def static_file(path):
+    print(path)
+    return send_file(path)
 
+@app.route(r'/')
+def root():
+    print("a")
+    return send_file('index.html')
+
+if __name__ is "__main__":
+    app.run()
